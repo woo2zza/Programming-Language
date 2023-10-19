2f76116bbf92a4110d57b71364b365afbce3c0d5
wooseok <dntjr5741@naver.com>
2023-10-19 18:00:29 +0900

    pjt06

---
+++ SSAFY_Class_django/20231018_serizlizers.1/10-01-django-rest-framework/articles/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from . models import Article
+
+class ArticleListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = ('id', 'title', 'contet',) ,
+
+
+class ArticleListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = '__all__'
+
+
---
+++ SSAFY_Class_project/project_movies_06/accounts/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+
+# Create your views here.
+def login(request):
+    pass
+
+def logout(request):
+
+    pass
+
+def signup(request):
+    pass
+
+def delete(request):
+    pass
+
+def update(request):
+    pass
+
+def change_password(request, user_pk):
+    pass
+
+def profile(request, username):
+    pass
+
+def follow(request, user_pk):
+    pass
+
