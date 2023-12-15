736327289867851043cacc4935f2f7791d68105a
simwooseok <dntjr5741@naver.com>
2023-12-16 01:29:10 +0900

    crud

--- React/simple_diary/src/App.js
+++ React/simple_diary/src/App.js
@@ -1,10 +1,35 @@
 import "./App.css";
+import { useRef, useState } from "react";
 import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const dataId = useRef(0);
+
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id: dataId.current,
+    };
+    dataId.current += 1;
+    setData([newItem, ...data]);
+  };
+
+  const onDelete = (targetId) => {
+    console.log(`${targetId}가 삭제 되었습니다`);
+    const newDiaryList = data.filter((it) => it.id !== targetId);
+    setData(newDiaryList);
+  };
   return (
     <div className="App">
-      <DiaryEditor />
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList diaryList={data} onDelete={onDelete} />
     </div>
   );
 }
--- React/simple_diary/src/DiaryEditor.js
+++ React/simple_diary/src/DiaryEditor.js
@@ -1,6 +1,9 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
-const DiaryEditor = () => {
+const DiaryEditor = ({ onCreate }) => {
+  const authorInput = useRef();
+  const contentInput = useRef();
+
   const [state, setState] = useState({
     author: "",
     content: "",
@@ -15,8 +18,23 @@ const DiaryEditor = () => {
   };
 
   const handleSubmit = () => {
-    console.log(state);
+    if (state.author.length < 1) {
+      authorInput.current.focus();
+      return;
+    }
+
+    if (state.content.length < 5) {
+      contentInput.current.focus();
+      return;
+    }
+
+    onCreate(state.author, state.content, state.emotion);
     alert("일기 저장하기");
+    setState({
+      author: "",
+      content: "",
+      emotion: 1,
+    });
   };
 
   return (
@@ -24,6 +42,7 @@ const DiaryEditor = () => {
       <h2>오늘의 일기</h2>
       <div>
         <input
+          ref={authorInput}
           name="author"
           value={state.author}
           onChange={handleChangeState}
@@ -31,6 +50,7 @@ const DiaryEditor = () => {
       </div>
       <div>
         <textarea
+          ref={contentInput}
           name="content"
           value={state.content}
           onChange={handleChangeState}
